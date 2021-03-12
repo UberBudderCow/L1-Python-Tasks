9f92181d79c2a88b75613c96d291fce57753773e
Points - Fergus Thamm <thammf@stu.otc.school.nz>
2021-03-12 13:09:26 +1300

    Added programme which calculates team scores

---
+++ Team scores.py
@@ -0,0 +1,11 @@
+first_score = 3
+second_score = 6
+third_score = 10
+fourth_score = 10
+fifth_score = 6
+print("First match score: {}".format(first_score))
+print("Second match score: {}".format(second_score))
+print("Third match score: {}".format(third_score))
+print("Fourth match score: {}".format(fourth_score))
+print("Fifth match score: {}".format(fifth_score))
+print("Total score is {}".format(first_score + second_score + third_score + fourth_score + fifth_score))
