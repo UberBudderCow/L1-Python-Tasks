f0242ea25a8ff72cbcf8206805b10979c183b373
Points - Fergus Thamm <thammf@stu.otc.school.nz>
2021-03-25 09:40:46 +1300

    haha funny gay test

---
+++ Gay test.py
@@ -0,0 +1,98 @@
+score = 0
+check = False
+print("How many sausages did you eat this month?")
+ans = input("a. 0, b. 1-2, c. 3-5, d. 5-10, e. 11+")
+
+while check == False:
+    if ans == "a":
+        print("Your response has been recorded")
+        check = True
+    elif ans == "b":
+        print("Your response has been recorded")
+        score += 2
+        check = True
+    elif ans == "c":
+        print("Your response has been recorded")
+        score += 5
+        check = True
+    elif ans == "d":
+        print("Your response has been recorded")
+        score += 10
+        check = True
+    elif ans == "e":
+        print("Your response has been recorded")
+        score += 20
+        check = True
+    else:
+        print("Please enter a, b, c, d or e")
+        ans = int(input())
+
+check = False
+print("What is your favourite colour?")
+ans = input("red, orange, yellow, green, blue, purple, pink, black, white, or rainbow?")
+
+while check == False:
+    if ans == "red" or ans == "yellow" or ans == "green" or ans == "blue" or ans == "purple" or ans == "pink":
+        print("Your response has been recorded")
+        check = True
+    elif ans == "orange":
+        print("Your response has been recorded")
+        score += 5
+        check = True
+    elif ans == "white" or ans == "black":
+        print("Your response has been recorded")
+        score += 10
+        check = True
+    elif ans == "rainbow":
+        print("Your response has been recorded")
+        score += 20
+        check = True
+    else:
+        print("Please enter one of the colour names shown")
+        ans = input()
+
+check = False
+print("What is your favourite 3D object?")
+ans = input("sphere, cube, pyramid, bean")
+
+while check == False:
+    if ans == "cube":
+        print("Your response has been recorded")
+        check = True
+    elif ans == "pyramid":
+        print("Your response has been recorded")
+        score += 5
+        check = True
+    elif ans == "sphere":
+        print("Your response has been recorded")
+        score += 10
+        check = True
+    elif ans == "bean":
+        print("Your response has been recorded")
+        score += 20
+        check = True
+    else:
+        print("Please enter one of the 3D shape names shown")
+        ans = input()
+
+check = False
+print("What is your favourite gender?")
+ans = input("male, female, other")
+
+while check == False:
+    if ans == "female":
+        print("Your response has been recorded")
+        check = True
+    elif ans == "other":
+        print("Your response has been recorded")
+        score += 10
+        check = True
+    elif ans == "male":
+        print("Your response has been recorded")
+        score += 20
+        check = True
+    else:
+        print("Please enter one of the genders shown")
+        ans = input()
+
+print(score)
