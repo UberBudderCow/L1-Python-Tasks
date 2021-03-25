8a68a5f2f7f86aa4b01252e9d2bd077cff9286d9
Points - Fergus Thamm <thammf@stu.otc.school.nz>
2021-03-25 14:16:49 +1300

    Finished gay test

--- Gay test.py
+++ Gay test.py
@@ -5,7 +5,7 @@ ans = input("a. 0, b. 1-2, c. 3-5, d. 5-10, e. 11+")
 
 while check == False:
     if ans == "a":
-        print("Your response has been recorded")
+        print("Your name has been recorded")
         check = True
     elif ans == "b":
         print("Your response has been recorded")
@@ -25,7 +25,7 @@ while check == False:
         check = True
     else:
         print("Please enter a, b, c, d or e")
-        ans = int(input())
+        ans = input()
 
 check = False
 print("What is your favourite colour?")
@@ -44,7 +44,7 @@ while check == False:
         score += 10
         check = True
     elif ans == "rainbow":
-        print("Your response has been recorded")
+        print("Your life has been recorded")
         score += 20
         check = True
     else:
@@ -52,7 +52,7 @@ while check == False:
         ans = input()
 
 check = False
-print("What is your favourite 3D object?")
+print("What is your favourite 8D object?")
 ans = input("sphere, cube, pyramid, bean")
 
 while check == False:
@@ -95,4 +95,37 @@ while check == False:
         print("Please enter one of the genders shown")
         ans = input()
 
-print(score)
+check = False
+print("To determine which question i ask, i will need to know your age")
+age = input("Please enter your age(This doesn't matter yet, just put what you want)")
+
+print("How many girlfriends have you had?")
+ans = input("a. 0, b. 1, c. 2-3, d. 4-5, e. 5+")
+
+
+while check == False:
+    if ans == "a":
+        print("Your response has been recorded")
+        score += 20
+        check = True
+    elif ans == "b":
+        print("Your response has been recorded")
+        score += 10
+        check = True
+    elif ans == "c":
+        print("Your response has been recorded")
+        score += 5
+        check = True
+    elif ans == "d":
+        print("Your response has been recorded")
+        score += 2
+        check = True
+    elif ans == "e":
+        print("Your response has been recorded")
+        score += 0
+        check = True
+    else:
+        print("Please enter a, b, c, d or e")
+        ans = input()
+
+print("You are {}% gay".format(score))
